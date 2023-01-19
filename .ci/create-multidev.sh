cf633b4aee4300a169186887554e25c2bc3441bf
Kevin Porras <kevin.porras@pantheon.io>
2023-01-19 08:01:52 -0600

    Do not touch the file if multidev creation fails.

--- .ci/create-multidev.sh
+++ .ci/create-multidev.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 terminus env:create $TERMINUS_SITE.$TERMINUS_BASE_ENV ${GITHUB_RUN_NUMBER}
+if [ $? -ne 0 ]; then
+  echo "Failed to create multidev environment"
+  exit 1
+fi
 touch multidev-made.txt
